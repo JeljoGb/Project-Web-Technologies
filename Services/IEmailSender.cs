c8bb2987f899ebe22efca8ad05ddd27abc761021
JeljoGb <jelle.laton@gmail.com>
2018-08-12 17:27:58 +0200

    Add project files.

---
+++ Services/IEmailSender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ConesOfAmazonshire.Services
+{
+    public interface IEmailSender
+    {
+        Task SendEmailAsync(string email, string subject, string message);
+    }
+}
